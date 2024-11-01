01827525d0053d871f7c3d76e831dff7c5969786
quyomega <quyomega@gmail.com>
2024-11-01 12:26:05 +0700

    1.0.1

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,18 @@ namespace baitaplon
 {
     public partial class Form1 : Form
     {
+        private ketnoi kn = new ketnoi();
         public Form1()
         {
             InitializeComponent();
+            dgSach.DataSource = kn.GetDataTable($"SELECT book_id,title,author,publisher,price,describe,quantity,category  FROM book;");
+            LoadData();
         }
+        private void LoadData()
+        {
+            dgSach.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgSach.DataSource = kn.GetDataTable("SELECT book_id AS [Mã sách], title AS [Tiêu đề], author AS [Tác giả], publisher AS [Nhà xuất bản], price AS [Giá], [describe] AS [Mô tả], quantity AS [Số lượng], category AS [Danh mục] FROM book;");
+        }
+        
     }
 }
---
+++ ketnoi.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baitaplon
+{
+    internal class ketnoi
+    {
+        private string kn = @"Data Source=(Localdb)\mssqlLocaldb;Initial Catalog=baitaplon;Integrated Security=True";
+        private SqlConnection sqlConnection = new SqlConnection();
+        public ketnoi()
+        {
+            try
+            {
+                sqlConnection.ConnectionString = kn;
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Kết nối thất bại ");
+                Application.Exit();
+            }
+        }
+        public int RunExecuteNonQuery(SqlCommand sqlCommand)
+        {
+            int result = 0;
+            try
+            {
+                sqlCommand.Connection = sqlConnection;
+                sqlConnection.Open();
+                result = sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Kết nối thất bại: " + ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close(); 
+            }
+            return result;
+        }
+        public DataTable GetDataTable(string query)
+        {
+            DataTable data = new DataTable();
+            try
+            {
+                sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); 
+                sqlDataAdapter.Fill(data);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Kết nối thất bại: " + ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+            return data;
+        }
+    }
+}
