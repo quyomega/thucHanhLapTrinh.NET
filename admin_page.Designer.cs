036389c58b68ef319fe4da71f084cb5c401efbd8
quyomega <quyomega@gmail.com>
2024-11-05 13:55:34 +0700

    Tìm kiếm ở ADMIN - quản lý sách

--- admin_page.Designer.cs
+++ admin_page.Designer.cs
@@ -31,14 +31,27 @@
             this.components = new System.ComponentModel.Container();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtPublisher = new System.Windows.Forms.TextBox();
+            this.txtCategory = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.dataGridViewBooks = new System.Windows.Forms.DataGridView();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.tabPage6 = new System.Windows.Forms.TabPage();
             this.tabPage7 = new System.Windows.Forms.TabPage();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.dataGridViewBooks = new System.Windows.Forms.DataGridView();
+            this.searchTimer = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbLocation = new System.Windows.Forms.ComboBox();
             this.tabControl1.SuspendLayout();
             this.tabPage5.SuspendLayout();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBooks)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,19 +67,116 @@
             this.tabControl1.Multiline = true;
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1256, 725);
+            this.tabControl1.Size = new System.Drawing.Size(1256, 706);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage5
             // 
+            this.tabPage5.Controls.Add(this.panel1);
             this.tabPage5.Controls.Add(this.dataGridViewBooks);
+            this.tabPage5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.tabPage5.Location = new System.Drawing.Point(4, 29);
             this.tabPage5.Name = "tabPage5";
             this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage5.Size = new System.Drawing.Size(1248, 692);
+            this.tabPage5.Size = new System.Drawing.Size(1248, 673);
             this.tabPage5.TabIndex = 4;
             this.tabPage5.Text = "Quản lý sách";
             this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cmbLocation);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtPrice);
+            this.panel1.Controls.Add(this.txtPublisher);
+            this.panel1.Controls.Add(this.txtCategory);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtName);
+            this.panel1.Location = new System.Drawing.Point(17, 387);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(529, 269);
+            this.panel1.TabIndex = 3;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(278, 170);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(193, 27);
+            this.txtPrice.TabIndex = 5;
+            this.txtPrice.TextChanged += new System.EventHandler(this.SearchCriteriaChanged);
+            // 
+            // txtPublisher
+            // 
+            this.txtPublisher.Location = new System.Drawing.Point(278, 122);
+            this.txtPublisher.Name = "txtPublisher";
+            this.txtPublisher.Size = new System.Drawing.Size(193, 27);
+            this.txtPublisher.TabIndex = 5;
+            this.txtPublisher.TextChanged += new System.EventHandler(this.SearchCriteriaChanged);
+            // 
+            // txtCategory
+            // 
+            this.txtCategory.Location = new System.Drawing.Point(278, 71);
+            this.txtCategory.Name = "txtCategory";
+            this.txtCategory.Size = new System.Drawing.Size(193, 27);
+            this.txtCategory.TabIndex = 5;
+            this.txtCategory.TextChanged += new System.EventHandler(this.SearchCriteriaChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(141, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Tìm kiếm theo giá";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tìm kiếm theo NXB";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tìm kiếm theo thể loại";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tìm kiếm theo tên";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(278, 14);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(193, 27);
+            this.txtName.TabIndex = 1;
+            this.txtName.TextChanged += new System.EventHandler(this.SearchCriteriaChanged);
+            // 
+            // dataGridViewBooks
+            // 
+            this.dataGridViewBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewBooks.Location = new System.Drawing.Point(17, 16);
+            this.dataGridViewBooks.Name = "dataGridViewBooks";
+            this.dataGridViewBooks.RowHeadersWidth = 51;
+            this.dataGridViewBooks.RowTemplate.Height = 24;
+            this.dataGridViewBooks.Size = new System.Drawing.Size(1216, 338);
+            this.dataGridViewBooks.TabIndex = 0;
             // 
             // tabPage3
             // 
@@ -114,21 +224,36 @@
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
-            // dataGridViewBooks
+            // searchTimer
             // 
-            this.dataGridViewBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewBooks.Location = new System.Drawing.Point(17, 85);
-            this.dataGridViewBooks.Name = "dataGridViewBooks";
-            this.dataGridViewBooks.RowHeadersWidth = 51;
-            this.dataGridViewBooks.RowTemplate.Height = 24;
-            this.dataGridViewBooks.Size = new System.Drawing.Size(1216, 338);
-            this.dataGridViewBooks.TabIndex = 0;
+            this.searchTimer.Interval = 500;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(151, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Tìm kiếm theo vị trí";
+            // 
+            // cmbLocation
+            // 
+            this.cmbLocation.FormattingEnabled = true;
+            this.cmbLocation.Items.AddRange(new object[] {
+            "Tất Cả",
+            "Tại Kệ",
+            "Tại Kho"});
+            this.cmbLocation.Location = new System.Drawing.Point(278, 223);
+            this.cmbLocation.Name = "cmbLocation";
+            this.cmbLocation.Size = new System.Drawing.Size(193, 28);
+            this.cmbLocation.TabIndex = 7;
             // 
             // admin_page
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1281, 749);
+            this.ClientSize = new System.Drawing.Size(1281, 730);
             this.Controls.Add(this.tabControl1);
             this.Name = "admin_page";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -136,6 +261,8 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing_1);
             this.tabControl1.ResumeLayout(false);
             this.tabPage5.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBooks)).EndInit();
             this.ResumeLayout(false);
 
@@ -151,6 +278,18 @@
         private System.Windows.Forms.TabPage tabPage7;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.DataGridView dataGridViewBooks;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Timer searchTimer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtPublisher;
+        private System.Windows.Forms.TextBox txtCategory;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbLocation;
     }
 }
 
--- admin_page.cs
+++ admin_page.cs
@@ -22,14 +22,42 @@ namespace baitaplon
             InitializeComponent();
             this.loginForm = form; // Gán tham chiếu
             LoadBooks();
+
+            // Sự kiện TextChanged cho TextBox tìm kiếm
+            txtName.TextChanged += txtSearch_TextChanged;
+            txtCategory.TextChanged += SearchCriteriaChanged;
+            txtPublisher.TextChanged += SearchCriteriaChanged;
+            txtPrice.TextChanged += SearchCriteriaChanged;
+
+            cmbLocation.SelectedIndexChanged += SearchCriteriaChanged;
+            // Sự kiện Tick cho Timer tìm kiếm
+            searchTimer.Tick += searchTimer_Tick;
         }
+
+        // Khi có thay đổi trong TextBox tìm kiếm
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            // Đặt lại Timer mỗi lần người dùng thay đổi nội dung
+            searchTimer.Stop();
+            searchTimer.Start();
+        }
+
+        // Xử lý tìm kiếm khi Timer hết hạn
+        private void searchTimer_Tick(object sender, EventArgs e)
+        {
+            // Dừng Timer để không thực hiện tìm kiếm nhiều lần
+            searchTimer.Stop();
+            // Gọi hàm tìm kiếm
+            SearchBooks();
+        }
+
         private void LoadBooks()
         {
             string connectionString = "Data Source=(Localdb)\\mssqlLocaldb;Initial Catalog=baitaplon;Integrated Security=True";
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 conn.Open();
-                string query = "SELECT book_id, book_name, category, publishing, price, quantityShelf, quantityStore FROM books"; // Bỏ cột img
+                string query = "SELECT book_id, book_name, category, publishing, price, quantityShelf, quantityStore FROM books";
                 SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
                 DataTable booksTable = new DataTable();
                 adapter.Fill(booksTable);
@@ -46,16 +74,66 @@ namespace baitaplon
                 dataGridViewBooks.Columns["quantityShelf"].HeaderText = "SL Trên Kệ";
                 dataGridViewBooks.Columns["quantityStore"].HeaderText = "SL Trong Kho";
 
-                // Thiết lập AutoSizeMode cho các cột
-                foreach (DataGridViewColumn column in dataGridViewBooks.Columns)
+                dataGridViewBooks.ColumnHeadersDefaultCellStyle.Font = new Font(dataGridViewBooks.Font, FontStyle.Bold);
+
+                dataGridViewBooks.Columns["book_id"].Width = 100;          // Chiều rộng cột "Mã Sách"
+                dataGridViewBooks.Columns["book_name"].Width = 140;       // Chiều rộng cột "Tên Sách"
+                dataGridViewBooks.Columns["category"].Width = 120;        // Chiều rộng cột "Thể Loại"
+                dataGridViewBooks.Columns["publishing"].Width = 150;      // Chiều rộng cột "Nhà Xuất Bản"
+                dataGridViewBooks.Columns["price"].Width = 90;           // Chiều rộng cột "Giá"
+                dataGridViewBooks.Columns["quantityShelf"].Width = 120;   // Chiều rộng cột "SL Trên Kệ"
+                dataGridViewBooks.Columns["quantityStore"].Width = 140;
+            }
+        }
+
+
+        private void SearchBooks()
+        {
+            string connectionString = "Data Source=(Localdb)\\mssqlLocaldb;Initial Catalog=baitaplon;Integrated Security=True";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT book_id, book_name, category, publishing, price, quantityShelf, quantityStore FROM books WHERE 1=1";
+
+                // Thêm các điều kiện tìm kiếm nếu có
+                List<SqlParameter> parameters = new List<SqlParameter>();
+
+                if (!string.IsNullOrEmpty(txtName.Text))
                 {
-                    //column.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells; 
-                    column.MinimumWidth = 150;
-                    //column.Width = Math.Min(column.Width, 200);
+                    query += " AND book_name LIKE @name";
+                    parameters.Add(new SqlParameter("@name", "%" + txtName.Text + "%"));
+                }
+                if (!string.IsNullOrEmpty(txtCategory.Text))
+                {
+                    query += " AND category LIKE @category";
+                    parameters.Add(new SqlParameter("@category", "%" + txtCategory.Text + "%"));
+                }
+                if (!string.IsNullOrEmpty(txtPublisher.Text))
+                {
+                    query += " AND publishing LIKE @publisher";
+                    parameters.Add(new SqlParameter("@publisher", "%" + txtPublisher.Text + "%"));
+                }
+                if (decimal.TryParse(txtPrice.Text, out decimal price))
+                {
+                    query += " AND price LIKE @price";
+                    parameters.Add(new SqlParameter("@price", price));
                 }
 
-                // Đảm bảo cột đầu tiên vẫn giữ độ rộng tự động
-                dataGridViewBooks.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells; // Cột đầu tiên là Mã Sách
+                string selectedLocation = cmbLocation.SelectedItem?.ToString();
+                if (selectedLocation == "Tại Kệ")
+                {
+                    query += " AND quantityShelf > 0";
+                }
+                else if (selectedLocation == "Tại Kho")
+                {
+                    query += " AND quantityStore > 0";
+                }
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                adapter.SelectCommand.Parameters.AddRange(parameters.ToArray());
+
+                DataTable booksTable = new DataTable();
+                adapter.Fill(booksTable);
+                dataGridViewBooks.DataSource = booksTable;
             }
         }
 
@@ -63,5 +141,12 @@ namespace baitaplon
         {
             loginForm.Show();
         }
+
+        private void SearchCriteriaChanged(object sender, EventArgs e)
+        {
+            searchTimer.Stop();
+            searchTimer.Start();
+        }
     }
+
 }
