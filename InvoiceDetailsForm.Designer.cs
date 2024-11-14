b4c6f11c2459b1e3f4bc26d5cdc44165cae95ebd
quyomega <quyomega@gmail.com>
2024-11-14 14:49:51 +0700

    FIX - font chi tiết hóa đơn

--- InvoiceDetailsForm.Designer.cs
+++ InvoiceDetailsForm.Designer.cs
@@ -37,18 +37,21 @@
             this.dgvInvoiceDetails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvInvoiceDetails.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgvInvoiceDetails.Location = new System.Drawing.Point(0, 0);
+            this.dgvInvoiceDetails.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgvInvoiceDetails.Name = "dgvInvoiceDetails";
             this.dgvInvoiceDetails.RowHeadersWidth = 51;
             this.dgvInvoiceDetails.RowTemplate.Height = 24;
-            this.dgvInvoiceDetails.Size = new System.Drawing.Size(878, 316);
+            this.dgvInvoiceDetails.Size = new System.Drawing.Size(660, 297);
             this.dgvInvoiceDetails.TabIndex = 0;
             // 
             // InvoiceDetailsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(878, 316);
+            this.ClientSize = new System.Drawing.Size(660, 297);
             this.Controls.Add(this.dgvInvoiceDetails);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "InvoiceDetailsForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "InvoiceDetailsForm";
--- InvoiceDetailsForm.cs
+++ InvoiceDetailsForm.cs
@@ -13,32 +13,31 @@ namespace baitaplon
 {
     public partial class InvoiceDetailsForm : Form
     {
-        private string selectedInvoiceId; // Biến để lưu mã hóa đơn
-        private Connect kn = new Connect(); // Đối tượng kết nối cơ sở dữ liệu
-
-        // Constructor nhận vào selectedInvoiceId từ AdminPage
+        private string selectedInvoiceId; 
+        private Connect kn = new Connect(); 
         public InvoiceDetailsForm(string invoiceId)
         {
             InitializeComponent();
-            this.selectedInvoiceId = invoiceId; // Lưu mã hóa đơn vào biến
-            LoadInvoiceDetails(invoiceId); // Gọi phương thức tải chi tiết hóa đơn
+            this.selectedInvoiceId = invoiceId; 
+            LoadInvoiceDetails(invoiceId);
+            SetInvoiceDetails();
+        }
+        private void SetInvoiceDetails()
+        {
+            dgvInvoiceDetails.ColumnHeadersDefaultCellStyle.Font = new Font(dgvInvoiceDetails.Font, FontStyle.Bold);
         }
 
-        // Phương thức để tải chi tiết hóa đơn
         private void LoadInvoiceDetails(string invoiceId)
         {
             string query = "SELECT MaChiTiet, MaHoaDon, MaSach, DonGia, SoLuong, TongGia FROM ChiTietHoaDon WHERE MaHoaDon = @invoiceId";
 
-            // Tạo tham số cho câu truy vấn
             SqlParameter[] parameters = new SqlParameter[]
             {
         new SqlParameter("@invoiceId", SqlDbType.NVarChar) { Value = invoiceId }
             };
 
-            // Lấy dữ liệu từ cơ sở dữ liệu
             DataTable invoiceDetailsTable = kn.GetDataTable(query, parameters);
 
-            // Gán dữ liệu vào DataGridView
             dgvInvoiceDetails.DataSource = invoiceDetailsTable;
         }
 
